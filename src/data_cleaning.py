ed4adefc7cc9403b644421b9e6ec6297a2411291
Emily <emedlin08@gmail.com>
2023-04-03 18:23:37 -0500

    actually done with data cleaning (i think)

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,13 +3,15 @@ this module is used to identify what data is
 necessary for the recommendation engine
 """
 
-# NOTE: to show link to games https://store.steampowered.com/app/2540 (last digits are the appid)
+
 import pandas as pd
 import re
+import json
 
 data = pd.read_csv('official_steam_games_list.csv')
 
 
+# remove columns that are not needed for the engine
 def remove_nonessential_cols():
     new_data = data.drop(
         columns=['publisher', 'userscore', 'owners', 'average_forever', 'owners', 'average_2weeks',
@@ -18,20 +20,21 @@ def remove_nonessential_cols():
 
 
 # rating is from 0-1, the closer to 1 the rating is the better
-def calculating_rating(data):
+def calculating_rating(df):
     ratings = []
-    for i in range(len(data)):
-        total_positive = data.loc[i, 'positive']
-        total_negative = data.loc[i, 'negative']
+    for i in range(len(df)):
+        total_positive = df.loc[i, 'positive']
+        total_negative = df.loc[i, 'negative']
         if total_negative + total_positive != 0:
             rating = total_positive / (total_negative + total_positive)
             ratings.append(rating)
         else:
             ratings.append(0)
-    data.insert(9, "rating_score", ratings, True)
-    return data
+    df.insert(9, "rating_score", ratings, True)
+    return df
 
 
+# makes sure that each game has a legit name and there's no random characters for an invalid name
 def contains_random_chars(df):
     # ensures that a title has 3 valid characters in succession
     pattern = re.compile('[^\u0001-\u007F]{3,}')
@@ -44,7 +47,39 @@ def contains_random_chars(df):
     return df
 
 
+# taking all the tags and putting it into an array for each row
+# this array will be added to a new dataframe column
+def collecting_tags_col(df):
+    list_of_tags = []  # collects arrays
+
+    for i in range(len(df)):
+        text = df.loc[i, 'tags']
+        text = text.replace("'", '"')  # correct json format
+        json_object = json.loads(text)
+        tags_per_row = []  # array of tags
+        # if the game does not have any tags, then its array is empty and skips it
+        if not bool(json_object):
+            list_of_tags.append([])
+            continue
+        # key is referring to the json key, I only need the name not the number
+        for key in json_object.keys():
+            tags_per_row.append(key)
+        list_of_tags.append(tags_per_row)  # this array collecting each row array
+
+    df.insert(10, "list of tags", list_of_tags, True)  # new column
+    return df
+
+
 revised_data = remove_nonessential_cols()
 revised_data = calculating_rating(revised_data)
-revised_data = contains_random_chars(revised_data)
 
+# for each row, ensure that the json is in the correct format
+for i in range(len(revised_data)):
+    data = revised_data.loc[i, 'tags']
+    json_str = data.replace("Beat 'em up", "Beat em up")  # take off '
+    json_str = json_str.replace("Shoot 'Em Up", "Shoot Em Up")  # take off '
+    json_str = re.sub(r'(?<!1990)\'', '"', json_str)  # replacing all ' with " except for 1990's
+    json_str = json_str.replace("1990's", "1990s")  # take off '
+    revised_data.at[i, 'tags'] = json_str  # put new json format into dataframe
+
+revised_data = collecting_tags_col(revised_data)
--- src/recommendation_engine.py
+++ src/recommendation_engine.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# NOTE: to show link to games https://store.steampowered.com/app/2540 (last digits are the appid)
+
 # recommendation engine has access to the new data
 import data_cleaning as dc
 
